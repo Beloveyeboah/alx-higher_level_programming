b877f9689249335599e7b424ddc9d0ca1aa6a009
beloveyeboah <beloveyeboah@gmail.com>
2023-08-08 16:39:40 -0700

    magic_calculation

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """prints the bytecode in python"""
+
+    if a < b:
+        return (c)
+    if c > b:
+        return (a)
+    return (a*b - c)
