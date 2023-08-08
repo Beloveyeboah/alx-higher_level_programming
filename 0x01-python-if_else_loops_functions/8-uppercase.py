c25f2a23e2383a481ba423c7cbc03b155aa28e7e
beloveyeboah <beloveyeboah@gmail.com>
2023-08-08 15:16:45 -0700

    print upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """Funtion prints the lowercase."""
+
+    uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
