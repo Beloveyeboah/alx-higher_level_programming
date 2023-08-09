ac6a9f583397821ce4b583754c6360e5be843295
beloveyeboah <beloveyeboah@gmail.com>
2023-08-09 11:26:09 -0700

    uppernow

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    """prints stings in uppercase"""
-
+    upcount = ""
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            i = chr(ord(i) - 32)
-        print("{}".format(i), end="")
-    print()
+        if  ord(i) >= 97 and ord(i) <= 122:
+            convert = chr(ord(i) - 32)
+            upcount += convert
+        else:
+            upcount += i
+    print(upcount)
