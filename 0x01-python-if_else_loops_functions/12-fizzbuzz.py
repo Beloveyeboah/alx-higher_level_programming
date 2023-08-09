1bfc6d5e38166038c15050d57834d55c956f4203
beloveyeboah <beloveyeboah@gmail.com>
2023-08-09 12:24:24 -0700

    fuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,6 @@
 
 """Funtion print numbers from 1 to 10"""
 
-
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    """convert sting to uppercae"""
     for i in str:
         if  ord(i) >= 97 and ord(i) <= 122:
-            i = chr(ord(i)  - 32)
+            i = chr(ord(i) - 32)
         print("{}".format(i), end="")
     print()
-
-
-my = "WE LOVE you more than him"
-uppercase(my)
