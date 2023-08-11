843cb225dad6e6e15d2d126eda70812a4429d1a5
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 07:57:44 -0700

    argument count

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+import sys
+read = len(sys.argv) - 1
+if read == 0:
+    print("0 arguments.")
+if read == 1:
+    print("1 argument: ")
+else:
+    print(f"{read} arguments:")
+for n in range(read):
+    print("{}: {}".format(n+1, sys.argv[n + 1]))
