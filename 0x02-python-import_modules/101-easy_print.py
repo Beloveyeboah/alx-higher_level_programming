ac2c12375807f8dd5b65f1d8e2e63f7778aea1f9
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 13:18:04 -0700

    fast uppercase

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+if __name__ == "__main__":
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+import string
+print(string.ascii_uppercase)
