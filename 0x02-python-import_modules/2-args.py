42d59fb9b303602b838e02205382be5b4f1b472e
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 11:33:26 -0700

    adding

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 # CREATED BY OSEI YEBOAH ISAAC
-
 if __name__ == "__main__":
-    """This function prints of the command line"""
 
     import sys
 
@@ -11,8 +9,8 @@ if __name__ == "__main__":
     if read == 0:
         print("0 arguments.")
     elif read == 1:
-        print("1 argument: ")
+        print("1 argument:")
     else:
         print("{} arguments:".format(read))
-    for n in range(read):
-        print("{}: {}".format(n + 1, sys.argv[n + 1]))
+    for args in range(read):
+        print("{}: {}".format(args + 1, sys.argv[args + 1]))
