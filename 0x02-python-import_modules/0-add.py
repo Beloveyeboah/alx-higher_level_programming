2b10fc5b9f1fcbf521b60ab4782a9583f0827685
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 06:08:25 -0700

    love adding

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+#CREATED: OSEI YEBOAH ISAAC
+
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    c = add(a, b)
-    print(f"{a} + {b} = {c}")
+    add(a, b)
+    print("{} + {} = {}".format(a, b, add(a,b), end=""))
