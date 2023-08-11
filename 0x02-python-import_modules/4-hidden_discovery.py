af312b99a7e5eba1d68fa359fe8abd60b696f283
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 12:03:05 -0700

    hidden_4_mod

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,11 @@
 
 # CREATED BY OSEI YEBOAH ISAAC
 if __name__ == "__main__":
+    """prints hidden msg in hidden_4"""
 
     import hidden_4
 
     names = dir(hidden_4)
     for x in names:
-        if x [:2] != "__":
+        if x[:2] != "__":
             print(x)
