499063aa2e9060302ff48745458a155ad1cdd65c
beloveyeboah <beloveyeboah@gmail.com>
2023-08-11 10:45:58 -0700

    last

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 
 # CREATED BY OSEI YEBOAH ISAAC
 
-import sys
-read = len(sys.argv) - 1
-if read == 0:
-    print("0 arguments.")
-if read == 1:
-    print("1 argument: ")
-else:
-    print(f"{read} arguments:")
-for n in range(read):
-    print("{}: {}".format(n+1, sys.argv[n + 1]))
+if __name__ == "__main__":
+    import sys
+    read = len(sys.argv) - 1
+    if read == 0:
+        print("0 arguments.")
+    if read == 1:
+        print("1 argument: ")
+    else:
+        print(f"{read} arguments:")
+    for n in range(read):
+        print("{}: {}".format(n+1, sys.argv[n + 1]))
