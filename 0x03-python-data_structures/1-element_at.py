172be5068352dd0d1db075171be160d2fb918fa9
beloveyeboah <beloveyeboah@gmail.com>
2023-08-13 20:45:06 -0700

    upt print ind

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,8 @@
 # CREATED BY OSEI YEBOAH ISAAC
 def element_at(my_list, idx):
     if idx < 0:
-        return (NULL)
-    elif idx > len(my_list) - 1:
-        return
+        return (None)
+    elif idx > (len(my_list) - 1):
+        return (None)
     else:
         return(my_list[idx])
