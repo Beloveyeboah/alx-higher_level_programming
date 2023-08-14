bde7646443236701810c9a63eccd0098c0e48ddd
beloveyeboah <beloveyeboah@gmail.com>
2023-08-13 21:39:15 -0700

    3-print_reversed_list_integer

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,6 @@
 # CREATED BY OSEI YEBOAH ISAAC
 
 def print_reversed_list_integer(my_list=[]):
+    """function prints in reversed oder of idx """
     for x in reversed(my_list):
         print("{:d}".format(x))
