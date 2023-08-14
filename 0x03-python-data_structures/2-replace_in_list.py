2d260f299dad4b694995f1c3c340c47d182cc8fd
beloveyeboah <beloveyeboah@gmail.com>
2023-08-13 21:19:10 -0700

    update of an element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 # CREATED BY OSEI YEBOAH ISAAC
 
 def replace_in_list(my_list, idx, element):
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
