1d5842d4e3716deda864ff7de1e77eaf16051f34
beloveyeboah <beloveyeboah@gmail.com>
2023-08-14 07:27:55 -0700

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+"""Write a function that finds the biggest integer of a list."""
+
+def max_integer(my_list=[]):
+    if my_list == None:
+        return None
+    if len(my_list) == 0:
+        return None
+    comp = my_list[0]
+    for i in my_list:
+        if i > comp:
+            comp = i
+    return (comp)
