88ccdc7069340ee5cd1e627544d618350efc7485
beloveyeboah <beloveyeboah@gmail.com>
2023-08-13 20:22:44 -0700

    prints

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@
 # CREATED BY OSEI YEBOAH ISAAC
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
+    for orn in my_list:
+        print("{:d}".format(orn))
