f117f4d378a78a3ca51c2fed59a8ae9e2406f40c
beloveyeboah <beloveyeboah@gmail.com>
2023-08-13 20:39:25 -0700

    printing index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+def element_at(my_list, idx):
+    if idx < 0:
+        return (NULL)
+    elif idx > len(my_list) - 1:
+        return
+    else:
+        return(my_list[idx])
