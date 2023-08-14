0182ab40da719452eb7cdd3d2231aaea00b22783
beloveyeboah <beloveyeboah@gmail.com>
2023-08-14 13:01:10 -0700

    print in matrix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 # CREATED BY OSEI YEBOAH ISAAC
 
+"""prints in reversed order"""
+
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         for item in my_list[::-1]:
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # CREATED BY OSEI YEBOAH ISAAC
+"""prints int in a matrix"""
 
 def print_matrix_integer(matrix=[[]]):
     if matrix:
