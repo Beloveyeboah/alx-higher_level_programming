8d06c94d5a03fb7b88f6d585784e961bb8265b43
beloveyeboah <beloveyeboah@gmail.com>
2023-08-14 07:33:39 -0700

    max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,10 @@
 
 """Write a function that finds the biggest integer of a list."""
 
+
 def max_integer(my_list=[]):
-    if my_list == None:
+
+    if my_list is None:
         return None
     if len(my_list) == 0:
         return None
