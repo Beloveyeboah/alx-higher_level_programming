3f92acdc5387b6cd77fbba9257e9af67bb5eb9be
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 14:51:25 -0700

    uniq add

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+def weight_average(my_list=[]):
+    """function that returns the weighted average"""
+
+
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,4 +8,3 @@ def uniq_add(my_list=[]):
     unique = list(set(my_list))
     result = sum(unique)
     return (result)
-
