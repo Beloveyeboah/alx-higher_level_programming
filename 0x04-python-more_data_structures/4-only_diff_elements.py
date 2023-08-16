9f9da4243cda86811167acfb0f2d18b96f698994
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 11:39:42 -0700

    difference of a  set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,8 @@
 def only_diff_elements(set_1, set_2):
     """function that returns a union of a set"""
 
-    return (set_1.union(set_2))
+    set_3 = set_1.union(set_2)
+    for i in set_2:
+        if i in set_1:
+            set_3.remove(i)
+    return (set_3)
