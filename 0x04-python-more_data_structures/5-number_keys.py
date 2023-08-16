21497177e5343da1726d123a2b47e33a4a36b751
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 11:49:31 -0700

    dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+def number_keys(a_dictionary):
+    """function that returns the number of keys in a dictionary"""
+
+    n = len(a_dictionary)
+    return (n)
