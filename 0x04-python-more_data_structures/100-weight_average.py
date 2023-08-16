4907a9a628f1e5cb6b900f8b49f0845ea8d28553
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 15:05:40 -0700

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,4 +5,14 @@
 def weight_average(my_list=[]):
     """function that returns the weighted average"""
 
-
+    if len(my_list) == 0:
+        return 0
+    w = 0
+    t = 0
+    for i in my_list:
+        score, weight = i
+        w += score * weight
+        t += weight
+    if t == 0:
+        return 0
+    return (w/t)
