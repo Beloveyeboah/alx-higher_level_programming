5a92239f1b3767bcf4a80c8f5e82a8f5ffe35858
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 09:46:55 -0700

    plus

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+def uniq_add(my_list=[]):
+    """convert to set and sum"""
+
+    unique = set(my_list)
+    result = sum(unique)
+    return (result)
+
