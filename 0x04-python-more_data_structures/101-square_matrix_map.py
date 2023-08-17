b63ac55175b34a119e599d22c5487cc121be25a1
beloveyeboah <beloveyeboah@gmail.com>
2023-08-16 17:28:30 -0700

    matrix

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+
+def square_matrix_map(matrix=[]):
+    """prints the matrix"""
+
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
