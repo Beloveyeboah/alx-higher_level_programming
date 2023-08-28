1c9ee78dcd2213dc910a971186931a69bf52e369
beloveyeboah <beloveyeboah@gmail.com>
2023-08-28 16:28:55 -0700

    adv

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+import sys
+
+def safe_print_integer_err(value):
+    """function that prints an integer."""
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return (False)
