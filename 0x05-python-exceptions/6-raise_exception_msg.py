4f5f8f3bf274eced31254b783e5464ef268e8e78
beloveyeboah <beloveyeboah@gmail.com>
2023-08-28 16:06:41 -0700

    message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# CREATED BY OSEI YEBOAH ISAAC
+
+def raise_exception_msg(message=""):
+    """function that raises a name exception with a message"""
+
+    raise NameError(message)
