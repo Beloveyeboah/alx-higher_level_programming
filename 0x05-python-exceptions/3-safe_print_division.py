042e2b3dc883cfac1713cea420f36cb1c5f5a57c
beloveyeboah <beloveyeboah@gmail.com>
2023-08-28 15:18:19 -0700

    div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,11 @@
 
 def safe_print_division(a, b):
     """function that divides 2 integers"""
-    c = a / b
-    try:
 
+    try:
+        c = a / b
+    except (TypeError, ZeroDivisionError):
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+    return (c)
