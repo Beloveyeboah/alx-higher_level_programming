af7445c5f35e6a9c7c2e5ef74a1f8a19f6017106
beloveyeboah <beloveyeboah@gmail.com>
2023-08-29 12:30:49 -0700

    upd 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,6 +14,8 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """calculating area"""
+
         return (self.__size * self.__size)
