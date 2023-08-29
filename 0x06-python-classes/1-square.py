2209dc278fd214cd93171e4f6ba1a87097193cf4
beloveyeboah <beloveyeboah@gmail.com>
2023-08-29 12:18:27 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Creating a class Square"""
+
+
+class Square:
+    """creating class of square"""
+
+    def __init__(self, size=0):
+        """self refrence"""
+
+        self.__size = size
