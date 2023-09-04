b50c8fc9cfa84f8dd1e6b1e3f9843433f1a51b1e
beloveyeboah <beloveyeboah@gmail.com>
2023-09-04 04:37:30 -0700

    rect

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# CREATED BY BELOVEYEBOAH
+
+class Rectangle:
+    """CREATES AN EMPTY RECTANGLE"""
+    pass
---
+++ 0x08-python-more_classes/a.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# CREATED BY BELOVEYEBOAH
+
+
