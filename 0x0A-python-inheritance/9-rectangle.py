8e39dd49c96999e2e796b263174874d9d91db67e
beloveyeboah <beloveyeboah@gmail.com>
2023-09-11 14:34:34 -0700

    area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""defines a base geometry"""
+
+# CREATED: BELOVE YEBOAH ISAAC
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a class to define rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """finds the area of rect"""
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """returns the print() and str() representation of a Rectangle"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
