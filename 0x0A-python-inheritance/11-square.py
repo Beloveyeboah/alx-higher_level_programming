2ccf5c9e3f273a78723ba79feb795fcba214b67d
beloveyeboah <beloveyeboah@gmail.com>
2023-09-11 14:59:33 -0700

    extra

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Define Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    function to find area of square"""
+
+    def __init__(self, size):
+        """arg: size"""
+
+        """Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
