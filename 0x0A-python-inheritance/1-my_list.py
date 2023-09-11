7ec3af221755e7cd904e61f8fb0d55fe9b17bf76
beloveyeboah <beloveyeboah@gmail.com>
2023-09-11 09:13:49 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class MyList that inherits from list"""
+
+# CREATED: BELOVE YEBOAH ISAAC
+
+
+class MyList(list):
+    """function inherits"""
+
+    def print_sorted(self):
+        """print list in ascend order"""
+
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defin a check claas function"""
+
+# CREATED: BELOVE YEBOAH ISAAC
+
+
+def is_same_class(obj, a_class):
+    """unction that returns True if the
+    object is exactly an instance"""
+
+    if type(obj) is a_class:
+        return True
+    return False
