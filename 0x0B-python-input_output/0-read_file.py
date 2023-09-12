e037bbf0bf33076ade61e9f078f39c4d155083cb
beloveyeboah <beloveyeboah@gmail.com>
2023-09-12 14:09:00 -0700

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a function to read text"""
+
+
+def read_file(filename=""):
+    """ reads from stdin"""
+
+    with open(filename, encoding="utf-8") as _file:
+        print(_file.read(), end=" ")
