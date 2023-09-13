f11248b0a3e4ca33310be9fccfc42ff4e7fae24b
beloveyeboah <beloveyeboah@gmail.com>
2023-09-12 17:29:32 -0700

    class student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""defines a class Student that defines a student by:"""
+
+
+class Student:
+    """ a class of students"""
+
+    def __init__(self, first_name, last_name, age):
+        """ imitiate the args"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrive from init"""
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload json"""
+
+        for k, v in json.items():
+                        setattr(self, k, v)
