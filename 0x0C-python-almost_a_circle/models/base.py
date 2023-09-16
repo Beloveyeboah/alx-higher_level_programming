4bc7c81ca0b247f9d708be93c3c8e8151580ac93
beloveyeboah <beloveyeboah@gmail.com>
2023-09-16 13:43:39 -0700

    up to 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 
 """define a class base fro this project"""
 
+import json
+
 
 class Base:
     """creates the bse class of the function"""
@@ -18,3 +20,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """initialize a json to strings"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if not isinstance(list_dictionaries, list):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        for item in list_dictionaries:
+            if not isinstance(item, dict):
+                raise TypeError("Each item in list must be a dictionary")
+        else:
+            return json.dumps(list_dictionaries)
