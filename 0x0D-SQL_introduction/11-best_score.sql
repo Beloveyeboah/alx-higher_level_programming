ba7328f6070180d8b1f00a5a2b34b1f9684e312a
beloveyeboah <beloveyeboah@gmail.com>
2023-10-17 13:16:05 -0700

    oder by

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- script that lists all records with a score >= 10
+-- Results should display both the score and the name (in this order)
+SELECT score, name FROM second_table ORDER BY score >10  DESC;
