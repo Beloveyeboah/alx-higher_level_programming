67a5fc5a9f7308814508db5d649706eac7f59074
beloveyeboah <beloveyeboah@gmail.com>
2023-10-17 13:55:18 -0700

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table second_table
+-- ecords should be listed by descending score
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
