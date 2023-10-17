edf74820988c3925d00e93160fe60a3c444daccc
beloveyeboah <beloveyeboah@gmail.com>
2023-10-17 12:58:58 -0700

    insert extra

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- script that creates a table second_table
-CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT) VALUES (1, "John", 10),
+CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
 (4, "George", 8);
