97010cc3a75d547ad1f4b10e02b12bd56e1e66d8
beloveyeboah <beloveyeboah@gmail.com>
2023-10-17 14:09:55 -0700

    go uft

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- script that converts hbtn_0c_0 database to UTF8
 
-ALTER TABLE hbtn_0c_0 CHARACTER
+ALTER DATABASE hbtn_0c_0 CHARACTER
 SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
