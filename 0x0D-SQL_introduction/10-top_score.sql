0dba08344b939f7d8ef6c85675c6b3bed4a01181
beloveyeboah <beloveyeboah@gmail.com>
2023-10-17 13:11:54 -0700

    list

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+SELECT score, name FROM second_table ORDER BY score DESC;
