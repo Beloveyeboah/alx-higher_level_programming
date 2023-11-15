bff52a7b7d9f9a5ba7be39ccf664e9ba7959f4f2
Beloveyeboah <beloveyeboah@gmail.com>
2023-11-14 16:07:29 -0800

    fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-let i = 0;
-const m = list.map(x => x * i++);
+const list = require('./100-data.js').list;
+
+const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(m);
+console.log(newList);
