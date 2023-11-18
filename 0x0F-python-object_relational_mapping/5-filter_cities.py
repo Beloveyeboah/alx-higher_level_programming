3c39dee5dc26681064168a9398739a53ff31636a
Beloveyeboah <beloveyeboah@gmail.com>
2023-11-18 06:55:07 -0800

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -29,10 +29,11 @@ if __name__ == "__main__":
                 ORDER BY cities.id ASC"
         cur.execute(query, (state_arg,))
         row = cur.fetchall()
-        for r in row:
-            print(r)
+        if row is not None:
+              print(", ".join([r[0] for r in row]))
         cur.close()
         conn.close()
+
 
     except MySQLdb.Error as e:
         print("Error connecting to MySQL database", str(e))
