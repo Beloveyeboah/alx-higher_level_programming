1e9930513d80e89125ba2737d38bb15d1ed2506a
beloveyeboah <beloveyeboah@gmail.com>
2024-03-11 09:26:45 -0700

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+ * The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
