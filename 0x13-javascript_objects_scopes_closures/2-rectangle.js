d9b19d5cc450f6510c1ae0607bff3f63eab5bb6a
beloveyeboah <beloveyeboah@gmail.com>
2024-03-12 10:20:00 -0700

    rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -9,7 +9,6 @@ Initialize the instance attribute width with the value of w
 Initialize the instance attribute height with the value of h
 If w or h is equal to 0 or not a positive integer, create an empty object
 */
-#!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
