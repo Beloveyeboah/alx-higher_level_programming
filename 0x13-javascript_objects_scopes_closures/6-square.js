30c87a964ece0da4508ce9674dcb4c1ec03978bc
beloveyeboah <beloveyeboah@gmail.com>
2024-03-12 11:34:48 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,16 +6,15 @@ const SquareEnt = require('./5-square');
 
 module.exports = class Square extends SquareEnt {
   charPrint (c) {
-	  for (let i = 0; i < this.height; i++)
-	  {
-		  if (!c) {
-			  console.log('X'.repeat(this.height));
-		  }
-		  if (c == 'C') {
-			  console.log('C'.repeat(this.height));
-		  }
-	  }
+    const display = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      let A = '';
+      let y = 0;
+      while (y < this.width) {
+        A += display;
+        y++;
+      }
+      console.log(A);
+    }
   }
-
 };
-
