fdc73efa4bc4a62ad68dfd9359cf5e3d3ba3c7ed
beloveyeboah <beloveyeboah@gmail.com>
2024-03-16 23:43:56 -0700

    to eleven

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects from the database hbtn_0e_6_usa
-
-Your script should take 3 arguments: mysql username, mysql password and database name
-You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-The results must be displayed as they are in the example below
-Your code should not be executed when imported
 """
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-
-
-import sys
+script that lists all State objects
+from the database hbtn_0e_6_usa"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """connects to the databases"""
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
