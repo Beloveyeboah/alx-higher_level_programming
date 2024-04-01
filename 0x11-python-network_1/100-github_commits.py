bf353b01635d3519b417d28536d941b90e12e3fe
beloveyeboah <beloveyeboah@gmail.com>
2024-04-01 13:21:14 -0700

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,12 +15,13 @@ import requests
 if __name__ == "__main__":
     repo = argv[1]
     user = argv[2]
-    url = f"https://api.github.com/repos/{repo}/{user}/commits"
-    num_of_commit = {'per_page': 10}
-    response = requests.get(url, num_of_commit)
-    if response.status_code == 200:
-        commits = response.json()
-        for i in commits:
-            sha = i['sha']
-            author = i['commit']['author']['name']
-            print("{}: {}".format(sha, author))
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo, user)
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
